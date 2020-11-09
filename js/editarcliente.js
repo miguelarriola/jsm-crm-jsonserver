bcdfc42905b4eab0645e018e086f3dce25053b51
Miguel Arriola <miguelarriola007@gmail.com>
2020-11-09 00:34:21 -0600

    primer commit

---
+++ js/editarcliente.js
@@ -0,0 +1,50 @@
+import { obtenerCliente, editarCliente } from './API.js';
+import { mostrarAlerta, validar } from './funciones.js';
+
+(function () {
+  const nombreInput = document.querySelector('#nombre');
+  const emailInput = document.querySelector('#email');
+  const empresaInput = document.querySelector('#empresa');
+  const telefonoInput = document.querySelector('#telefono');
+  const idInput = document.querySelector('#id');
+
+  document.addEventListener('DOMContentLoaded', async () => {
+    const parametrosURL = new URLSearchParams(window.location.search);
+
+    const idCliente = parseInt(parametrosURL.get('id'));
+
+    const cliente = await obtenerCliente(idCliente);
+    mostrarCliente(cliente);
+
+    const formulario = document.querySelector('#formulario');
+    formulario.addEventListener('submit', validarCliente);
+  });
+
+  function mostrarCliente(cliente) {
+    const { nombre, empresa, email, telefono, id } = cliente;
+    nombreInput.value = nombre;
+    emailInput.value = email;
+    empresaInput.value = empresa;
+    telefonoInput.value = telefono;
+    idInput.value = id;
+  }
+
+  function validarCliente(e) {
+    e.preventDefault();
+
+    const cliente = {
+      nombre: nombreInput.value,
+      email: emailInput.value,
+      telefono: telefonoInput.value,
+      empresa: empresaInput.value,
+      id: parseInt(idInput.value),
+    };
+
+    if (validar(cliente)) {
+      mostrarAlerta('Todos los campos son obligatorios');
+      return;
+    }
+
+    editarCliente(cliente);
+  }
+})();
